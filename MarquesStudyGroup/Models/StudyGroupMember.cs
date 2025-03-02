bc53c7238c416a42d9ca5733a96b3fa19c77f051
Marques Jaboon <u23649284@up.ac.za>
2025-03-02 19:11:41 +0200

    1

--- MarquesStudyGroup/Models/StudyGroupMember.cs
+++ MarquesStudyGroup/Models/StudyGroupMember.cs
@@ -5,8 +5,11 @@ using System.Web;
 
 namespace MarquesStudyGroup.Models
 {
-	public class StudyGroupMember
-	{
-
-	}
+    public class StudyGroupMember
+    {
+        public string StudentNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+    }
 }
